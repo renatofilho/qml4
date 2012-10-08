1d919c163af58de0357b5875979ccad1807521d1
Renato Araujo Oliveira Filho <renato@canonical.com>
2012-10-08 09:43:39 -0300

    Added moc functions for small_object.

---
+++ qml/moc_small_object.cxx
@@ -0,0 +1,76 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'small_object.h'
+**
+** Created: Mon Oct 8 09:37:17 2012
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "small_object.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'small_object.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_SmallObject[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_SmallObject[] = {
+    "SmallObject\0"
+};
+
+void SmallObject::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData SmallObject::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject SmallObject::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_SmallObject,
+      qt_meta_data_SmallObject, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &SmallObject::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+void *SmallObject::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_SmallObject))
+        return static_cast<void*>(const_cast< SmallObject*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int SmallObject::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
